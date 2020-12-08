65fa3d427412eb15c4291b4e2f550d67de1ce424
Jason <jvongsana@hotmail.com>
2020-12-08 12:15:14 -0800

    printing name and links for first page

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
+# make chrome headless
 options = Options()
 options.headless = True
-options.add_argument("--window-size=1920,1200")
+options.add_argument("--window-size=1920,1080")
 
-DRIVER_PATH = '/home/vongsana/projects/web-scraper/chromedriver'
+# chrome v87
 options.binary_location = '/usr/bin/google-chrome-stable'
 
+# chromedriver v87
+DRIVER_PATH = '/home/vongsana/projects/web-scraper/chromedriver'
+
+# run chrome and get craigslist page
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get('https://vancouver.craigslist.org/d/for-sale/search/sss?sort=date&query=pokemon')
-print(driver.page_source)
+
+#parse html
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+#all results for page
+for post in soup.find_all('a', class_='result-title'):
+  print(post.text + ' - ' + post['href'])
+
+
+
 driver.quit()
