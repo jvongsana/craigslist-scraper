d0c572c2a337031c8c2b4cc990f7fc0ca663941c
Jason <jvongsana@hotmail.com>
2020-12-09 13:06:55 -0800

    prints all posts on all pages under search

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ options.binary_location = '/usr/bin/google-chrome-stable'
 
 # chromedriver v87
 DRIVER_PATH = '/home/vongsana/projects/web-scraper/chromedriver'
+BASE_URL = 'https://vancouver.craigslist.org'
 
 # run chrome and get craigslist page
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
@@ -21,9 +22,21 @@ driver.get('https://vancouver.craigslist.org/d/for-sale/search/sss?sort=date&que
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 #all results for page
-for post in soup.find_all('a', class_='result-title'):
-  print(post.text + ' - ' + post['href'])
 
+
+
+def nextPage(pageLink):
+  driver.get(BASE_URL + pageLink)
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+  for post in soup.find_all('a', class_='result-title'):
+    print(post.text + ' - ' + post['href'])
+    
+  nextButton = soup.find('a', class_='next')
+  if nextButton is None: return
+  nextPage(nextButton.get('href'))
+
+nextPage('/d/for-sale/search/sss?sort=date&query=pokemon')
 
 
 driver.quit()
