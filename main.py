0b2bf2d84e0b6f44a7c6baa7b5963ff66b593c59
Jason <jvongsana@hotmail.com>
2020-12-09 22:57:32 -0800

    all posts on all page with ellapsed time posted

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from datetime import datetime
+
 
 # make chrome headless
 options = Options()
@@ -12,25 +14,37 @@ options.binary_location = '/usr/bin/google-chrome-stable'
 
 # chromedriver v87
 DRIVER_PATH = '/home/vongsana/projects/web-scraper/chromedriver'
-BASE_URL = 'https://vancouver.craigslist.org'
 
 # run chrome and get craigslist page
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
+BASE_URL = 'https://vancouver.craigslist.org'
+QUERY = 'pokemon'
+
 #all results on all pages
+allPosts = []
+
 def startSearch(pageLink):
   driver.get(BASE_URL + pageLink)
   soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-  for post in soup.find_all('a', class_='result-title'):
-    print(post.text + ' - ' + post['href'])
-
+  allPosts.extend(soup.find_all('li', 'result-row'))
   nextButton = soup.find('a', class_='next')
-  if nextButton is None: return
+  if nextButton is None: return allPosts
   startSearch(nextButton.get('href'))
 
+startSearch(f'/d/for-sale/search/sss?sort=date&query={QUERY}')
 
-startSearch('/d/for-sale/search/sss?sort=date&query=pokemon')
+
+for i, post in enumerate(allPosts):
+  postTime = post.find('time').get('datetime')
+  postTimeFormatted = datetime.strptime(postTime, '%Y-%m-%d %H:%M')
+  ellapsedTime = (datetime.now() - postTimeFormatted)
+  postTitle = post.find('a', class_='result-title').get_text()
+  postURL = post.find('a', class_='result-title')['href']
+
+  print(f'{i} - {ellapsedTime}:    {postTitle} - {postURL}')
+  
 
 
 driver.quit()
+  # for post in soup.find_all('li', class_='result-row'):
